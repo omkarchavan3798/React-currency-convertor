4f6ec586b2e7f9f1f64a24baf2dc549ddede961a
omkarchavan3798 <omkarchavan3798@gmail.com>
2024-07-14 21:11:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CurrencyConverter from './Components/CurrencyConverter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
